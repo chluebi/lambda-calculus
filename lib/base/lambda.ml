a3e9b9142da53cc320b9b521081bede8c9efc8ac
chluebi <42419603+chluebi@users.noreply.github.com>
2024-06-29 00:18:56 +0200

    lambda calculus implemented

--- lib/base/lambda.ml
+++ lib/base/lambda.ml
@@ -2,33 +2,76 @@ module LambdaCalculus = struct
   type var = string
   type t = Lamb of var * t | App of t * t | Var of var
 
-  let rec lambda_fold (lamb_f : (var * 'a) -> 'a) (app_f : ('a * 'a) -> 'a) (var_f : var -> 'a) (tree : t) : 'a =
+  let rec lambda_fold (lamb_f : var * 'a -> 'a) (app_f : 'a * 'a -> 'a)
+      (var_f : var -> 'a) (tree : t) : 'a =
     match tree with
-      | Lamb (v, b) -> lamb_f (v, (lambda_fold lamb_f app_f var_f b))
-      | App (f, a) -> app_f (lambda_fold lamb_f app_f var_f f, lambda_fold lamb_f app_f var_f a)  
-      | Var v -> var_f v
+    | Lamb (v, b) -> lamb_f (v, lambda_fold lamb_f app_f var_f b)
+    | App (f, a) ->
+        app_f
+          (lambda_fold lamb_f app_f var_f f, lambda_fold lamb_f app_f var_f a)
+    | Var v -> var_f v
 
   let print (tree : t) : string =
-    lambda_fold 
+    lambda_fold
       (fun (var, t) -> "(\\" ^ var ^ " => " ^ t ^ ")")
-      (fun (f, a) -> f ^ "" ^ a)
-      (fun v -> v) 
+      (fun (f, a) -> "(" ^ f ^ " " ^ a ^ ")")
+      (fun v -> v)
       tree
 
-  let rec sub (sub_v: var) (sub_expr: t) (tree:  t) : t =
+  let rec sub (sub_v : var) (sub_expr : t) (tree : t) : t =
     match tree with
-      | Lamb (v, b) -> if sub_v = v then Lamb (v, b) else Lamb (v, sub sub_v sub_expr b)
-      | App (f, a) -> App ((sub sub_v sub_expr f), (sub sub_v sub_expr a))
-      | Var v -> if sub_v = v then sub_expr else Var v
+    | Lamb (v, b) ->
+        if sub_v = v then Lamb (v, b) else Lamb (v, sub sub_v sub_expr b)
+    | App (f, a) -> App (sub sub_v sub_expr f, sub sub_v sub_expr a)
+    | Var v -> if sub_v = v then sub_expr else Var v
 
+  let rec eval_step_eager (tree : t) : t * bool =
+    match tree with
+      | Lamb _ -> tree, false
+      | App (f, a) -> (
+          match eval_step_eager f with
+            | (f, false) -> (
+              match eval_step_eager a with
+                | (a, false) -> (
+                  match f with
+                    | Lamb (v, b) -> sub v a b, true
+                    | _ ->  raise (Failure "Only lambdas can take arguments")
+                )
+                | (a, true) -> App (f, a), true
+              )
+            | (f, true) -> App (f, a), true
+      )
+      | Var _ -> tree, false
+
+  let rec eval_eager (tree: t) : t =
+    match eval_step_eager tree with
+      | (tree', true) -> eval_eager tree'
+      | (tree', false) -> tree'
+
+
+  let rec eval_step_lazy (tree : t) : t * bool =
+    match tree with
+      | Lamb _ -> tree, false
+      | App (f, a) -> (
+          match eval_step_lazy f with
+            | (f, false) -> (
+              match f with
+                | Lamb (v, b) -> sub v a b, true
+                | _ ->  raise (Failure "Only lambdas can take arguments")
+              )
+            | (f, true) -> App (f, a), true
+      )
+      | Var _ -> tree, false
+
+  let rec eval_lazy (tree: t) : t =
+    match eval_step_lazy tree with
+      | (tree', true) -> eval_lazy tree'
+      | (tree', false) -> tree'
 end
 
-
 module Examples = struct
-
   open LambdaCalculus
 
   let example1 : t = Var "x"
   let example2 : t = App (Lamb ("x", Var "x"), Lamb ("x", Var "x"))
-
 end
--- test/base/structure.ml
+++ test/base/structure.ml
@@ -1,1 +1,27 @@
-let () = print_endline ""
+open Base.Lambda.LambdaCalculus
+
+let () =
+  let tree1 = Var "x" in
+  let _ = print_endline (print tree1) in
+  let tree2 = App (Lamb ("x", Var "x"), Lamb ("x", Var "x")) in
+  let _ = print_endline (print tree2) in
+  let _ = print_endline (print (match eval_step_eager tree2 with | (f, _) -> f)) in
+  let _ = print_endline (print (eval_eager tree2)) in
+  let tree3 = App (Lamb ("x", Lamb ("y", App (Var "x", Var "y"))), Lamb ("x", Var "x")) in
+  let _ = print_endline (print tree3) in
+  let _ = print_endline (print (match eval_step_eager tree3 with | (f, _) -> f)) in
+  let _ = print_endline (print (eval_eager tree3)) in
+  let tree4 = App (tree3, Lamb ("z", Var "z")) in
+  let _ = print_endline (print tree4) in
+  let _ = print_endline (print (match eval_step_eager tree4 with | (f, _) -> f)) in
+  let _ = print_endline (print (eval_eager tree4)) in
+  let tree5 = App (Lamb ("x", App (Var "x", Var "x")), Lamb ("x", App (Var "x", Var "x"))) in
+  let _ = print_endline (print tree5) in
+  let _ = print_endline (print (match eval_step_eager tree5 with | (f, _) -> f)) in
+  let tree6 = App (Lamb ("x", Var "x"), App (Lamb ("y", Var "y"), Lamb ("z", Var "z"))) in
+  let _ = print_endline (print tree6) in
+  let _ = print_endline (print (match eval_step_eager tree6 with | (f, _) -> f)) in
+  let _ = print_endline (print (eval_eager tree6)) in
+  let _ = print_endline (print (match eval_step_lazy tree6 with | (f, _) -> f)) in
+  let _ = print_endline (print (eval_lazy tree6)) in
+  print_endline ""
